8560a959dce0b4f578d748e0dede380fac9f08d8
D4vid999 <77785558+D4vid999@users.noreply.github.com>
2022-09-28 17:23:51 -0500

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,145 @@
+function normal1() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="También conocida como la bella alfombra, esta semilla se encuentra en la vereda El Barro. Se caracteriza por cubrir grandes espacios y ofrecer una floración bastante interesante. Ofrece varias ventajas frente al césped, como es su crecimiento rápido y su bajo mantenimiento."
+
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+	mostrar2.style.background="url('lip.png')"
+
+
+}
+function normal2() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="También llamada Almáciga, esta semilla se ubica en la vereda El Barro. Es un cultivo tan rústico y resistente que un Lentisco es capaz de crecer y desarrollarse sin intervención humana."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+	mostrar2.style.background="url('lenti.png')"
+
+
+}
+function normal3() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="Esta semilla está ubicada en la vereda Potrerito, ha sido  empleada como fuente de vibra, para la fabricación de empaques."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.background="url('xd1.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
+function normal4() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="Esta semilla está ubicada en la vereda Sadra. El Canelo es  utilizado para la tos y catarro, lombrices intestinales,  desórdenes estomacales, disentería y dolores reumáticos."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.background="url('xd2.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
+function normal5() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="También conocida como Eneldo, está ubicada en la vereda Holanda Baja. Es buena para combatir flatulencias,  antiespasmódico y antidispéptico, en resúmen, vuelve las digestiones livianas y evita molestias estomacales."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.background="url('xd3.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
+function normal6() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="Ubicada en la vereda San Diego, es conocida por sus múltiples propiedades antibacteriales, antimicóticas, andiparasitarias y nutricionales debido a sus características químicas y bromatológicas. La pulpa de su fruto tiene alto contenido de fibra rica en sustancias antioxidantes y con alta capacidad de absorción de agua."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.background="url('pepe.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
+function normal7() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="También conocida como cedrón, esta planta está ubicada en la  vereda El Totumo. Sus hojas son muy aromáticas, han servido  para la industria alimentaria y para las farmacias, así como en las perfumerías y cosméticos. Se utiliza e la preparación de  bebidas y dulces."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.background="url('xd5.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
+function normal8() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="También conocida como Matalahúva, esta planta está ubicada en lavereda El Totumo. Tiene propiedades antibacterianas y antifúngicas, el anís estrellado alivia la congestión y limpialas vías respiratorias. Y el té de anís estrellado es muy recomendado para personas resfriadas. Diversos jarabes y remedios se hacen a partir de esta planta."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=""
+	mostrar2.style.background="url('xd6.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
+function normal9() {
+	mostrar.style.width=0+"px"
+	mostrar.style.height=0+"px"
+	mostrar.innerHTML="También conocida como el Platanillo, esta semilla está ubicada en la vereda Potrerito. Se utiliza en el tratamiento de la impotencia, infecciones vaginales y cistitis."
+	mostrar.style.width=500+"px"
+	mostrar.style.height=300+"px"
+
+	mostrar2.style.width=0+"px"
+	mostrar2.style.height=0+"px"
+	mostrar2.innerHTML=" "
+	mostrar2.style.background="url('xd7.png')"
+	mostrar2.style.width=300+"px"
+	mostrar2.style.height=300+"px"
+
+
+}
---
+++ java2.js
@@ -0,0 +1,28 @@
+function inicio() {
+	  mostrar.style.width=500+"px"
+	  mostrar.style.height=400+"px"
+	  mostrar2.style.width=360+"px"
+	  mostrar2.style.height=260+"px"
+	  mostrar2.style.background="url('bunk.png')"
+	  	setTimeout(adios,14000)
+	  mostrar.innerHTML="Las semillas se han guardado dentro de un bunker por si llega a aparecer una catástrofe mundial, al parecer debemos hacer una simulación para confirmar de que las personas sobrevivientes puedan encontrar este bunker por medio de las coordenadas dadas y que puedan sobrevivir con cada ejemplar de semilla almacenada."
+}
+function adios() {
+	  mostrar.style.display="none"
+      mostrar3.innerHTML="La simulación ha empezado, una catástrofe nuclear hizo que la mayoría de las plantas estén contaminadas y ningún ser humano sea capaz de soportar la radioactividad de estas mismas, pasados unos meses, los seres humanos podrán salir de nuevo a cultivar las nuevas plantas y así empezar desde 0."
+	  mostrar3.style.width=500+"px"
+	  mostrar3.style.height=400+"px"
+	   mostrar2.style.width=245+"px"
+	  mostrar2.style.height=220+"px"
+	 setTimeout(hola,14000)
+
+	  mostrar2.style.background="url('nuk.png')"
+}
+function hola() {
+    mostrar3.style.display="none"
+    mostrar5.innerHTML="Después de unos años la tierra volvió a su normalidad y la vida humana prevalece, la simulación fue todo un éxito."
+    mostrar5.style.width=500+"px"
+	mostrar5.style.height=400+"px"
+
+	  mostrar2.style.background="url('planeta.png')"
+}
